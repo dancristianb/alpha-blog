c03c74fcd2193136f1eed401cd69d1a12bd86700
dan <dan.cristian.bota@gmail.com>
2020-06-01 15:10:12 +0300

    146. Add users index

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -3,6 +3,6 @@ module UserHelper
     email_address = user.email.downcase
     hash = Digest::MD5.hexdigest(email_address)
     image_src = "https://www.gravatar.com/avatar/#{hash}?s=#{options[:size]}"
-    image_tag(image_src, alt: @user.username, class: 'rounded mx-auto d-block mt-4 shadow')
+    image_tag(image_src, alt: user.username, class: 'rounded mx-auto d-block mt-4 shadow')
   end
 end
