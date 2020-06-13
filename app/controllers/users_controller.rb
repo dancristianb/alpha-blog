4748ec1120d1b5c8cf736b25a628d24dd37f6175
dan <dan.cristian.bota@gmail.com>
2020-06-13 13:47:16 +0300

    oups sign up restricted to logged in users only

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[edit update show]
-  before_action :require_user, except: %i[index show]
+  before_action :require_user, only: %i[edit update destroy]
   before_action :require_same_user, only: %i[edit update destroy]
 
   def index
