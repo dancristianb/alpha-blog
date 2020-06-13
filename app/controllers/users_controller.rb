2db11f10a027481544b406137b578bbb2b869e07
dan <dan.cristian.bota@gmail.com>
2020-06-13 13:43:11 +0300

    167. Restrict actions for users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[edit update show]
+  before_action :require_user, except: %i[index show]
+  before_action :require_same_user, only: %i[edit update destroy]
 
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -42,5 +44,12 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def require_same_user
+    unless current_user == @user
+      flash[:alert] = 'You can only edit your own account!'
+      redirect_to users_path
+    end
   end
 end
