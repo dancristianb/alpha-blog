5d25f104ee264d60a10fd08121a8e97c3be15cde
dan <dan.cristian.bota@gmail.com>
2020-07-21 23:11:57 +0300

    205. Edit categories
    + extract set_category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,12 @@
 class CategoriesController < ApplicationController
   before_action :require_admin, except: [:index, :show]
+  before_action :set_category, only: [:show, :edit, :update]
 
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
   end
 
   def show
-    @category = Category.find(params[:id])
     @articles = @category.articles.paginate(page: params[:page], per_page: 2)
   end
 
@@ -24,7 +24,20 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def update
+    if @category.update(category_params)
+      flash[:notice] = "Cateogry name updated successfully!"
+      redirect_to @category
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
 
   def category_params
     params.require(:category).permit(:name)
